f1edb77b70c2d7ffdf5df296170ffe85713763f8
AndriiKom94 <andrii.kom@ukr.net>
2021-01-24 19:35:05 +0200

    Corrected

--- Word.py
+++ Word.py
@@ -2,7 +2,6 @@ from __future__ import print_function
 from mailmerge import MailMerge
 import xlrd
 import Doctors
-#from docx2pdf import convert
 from subprocess import Popen
 loc = ('REPORT.xls')
 
@@ -23,7 +22,7 @@ for row in range(sheet.nrows):
             doctor=Doctors.res[str(int((value[1])))],
             invoice=str(int(value[0])),
             patient=value[4],
-            DOB=value[5],
+            DOB=value[5][1:],
             code=str(int(value[1]))
         )
 
@@ -35,25 +34,8 @@ for row in range(sheet.nrows):
             doctor='NOT FOUND',
             invoice=str(int(value[0])),
             patient=value[4],
-            DOB=value[5],
+            DOB=value[5][1:],
             code=str(int(value[1]))
         )
         document.write('RESULT/NotFound/ERROR'+str(int(value[1]))+'_'+str(int(value[0]))+'.docx')
-
-#convert('RESULT/', 'RESULT/PDFs/')
-
-
-# LIBRE_OFFICE = r"C:\Program Files\LibreOffice\program\soffice.exe"
-#
-# def convert_to_pdf(input_docx, out_folder):
-#     p = Popen([LIBRE_OFFICE, '--headless', '--convert-to', 'pdf', '--outdir',
-#                out_folder, input_docx])
-#     print([LIBRE_OFFICE, '--convert-to', 'pdf', input_docx])
-#     p.communicate()
-#
-#
-# sample_doc = 'file.docx'
-# out_folder = 'some_folder'
-# convert_to_pdf(sample_doc, out_folder)
-
 
